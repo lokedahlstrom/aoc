30e26ba02bf81a757a112b04126b6a89bfcab95f
Loke Dahlström <L21390@eon.com>
2021-12-04 22:47:12 +0100

    added helpers and renamed some stuff

---
+++ 2021/helpers/index.js
@@ -0,0 +1,11 @@
+import { readFileSync } from 'fs'
+
+export const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+export const range = n => [...Array(n).keys()]
+export const sum = v => v.reduce((acc, x) => acc + x, 0)
+export const empty = v => v === null || v === undefined || v === ''
+export const notEmpty = v => !empty(v)
