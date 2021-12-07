54c226cbf6194081eacedd539b30d267be85ffd8
Loke Dahlström <L21390@eon.com>
2021-12-07 17:51:27 +0100

    Day 7, problem 2 solved

--- 2021/07/2.js
+++ 2021/07/2.js
@@ -1,15 +1,15 @@
-import { read, ints, range, sum } from '../helpers'
+import { readInts, sum } from '../helpers'
 
 const mean = arr => sum(arr) / arr.length
 const steps = (source, dest) => Math.abs(source - dest)
 const arithmeticSum = n => n *((1 + n) / 2)
 
 const solve = data => {
-  const m = mean(data)
-  const result = data.reduce((acc, n) => acc + arithmeticSum(steps(d, m)), 0)
+  const m = Math.floor(mean(data))
+  const result = data.reduce((acc, n) => acc + arithmeticSum(steps(n, m)), 0)
 
   return `Result: ${result}`
 }
 
-console.log("Test", solve(read('./test')))
-console.log("Data", solve(read('./data')))
+console.log("Test", solve(readInts('./test')))
+console.log("Data", solve(readInts('./data')))
