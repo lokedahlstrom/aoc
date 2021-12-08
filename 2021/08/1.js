412c0bc76f73c9c73bdba4d8d0a507cca4e43f64
Loke Dahlström <L21390@eon.com>
2021-12-08 10:03:09 +0100

    Day 8, problem 1 solved

--- 2021/08/1.js
+++ 2021/08/1.js
@@ -1,9 +1,20 @@
-import { readInts } from '../helpers'
+import { read } from '../helpers'
+
+const decodeRow = d => {
+  const [_, signal_input] = d.split('|')
+  const signals = signal_input.trim().split(' ')
+  return signals.filter(a => 
+    a.length === 2 ||
+    a.length === 3 || 
+    a.length === 4 || 
+    a.length === 7
+  ).length
+}
 
 const solve = data => {
-  const result = 0
+  const result = data.reduce((acc, r) => acc + decodeRow(r), 0)
   return `Result: ${result}`
 }
 
-console.log("Test", solve(readInts('./test')))
-console.log("Data", solve(readInts('./data')))
+console.log("Test", solve(read('./test')))
+console.log("Data", solve(read('./data')))
