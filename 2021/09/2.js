ffc2aa0de8cf6a76d18db062d667e8d57b640dfa
Loke Dahlström <L21390@eon.com>
2021-12-09 16:10:50 +0100

    Day 9, problem 1 and 2 solved

--- 2021/09/2.js
+++ 2021/09/2.js
@@ -1,9 +1,87 @@
-import { readInts } from '../helpers'
+import { read } from '../helpers'
+
+const first = v => v[0]
+
+const get = (matrix, rows, rowLength) => (x,y) => {
+  if (y < 0 || y >= rows)
+    return 99999
+  if (x < 0 || x >= rowLength)
+    return 99999
+  return matrix[y][x]
+}
+
+const isLowest = (matrix, rows, rowLength) => (x, y) => {
+  const mget = get(matrix, rows, rowLength)
+
+  const P = mget(x, y)
+  const N = mget(x, y-1)
+  const W = mget(x-1, y)
+  const E = mget(x+1, y)
+  const S = mget(x, y+1)
+
+  return P < Math.min(N, W, E, S)
+}
+
+const p2s = v => JSON.stringify(v)
+const has = (s, v) => s.has(p2s(v))
+
+const oob = (matrix, rows, rowLength) => (x, y) => {
+  if (y < 0 || y >= rows)
+    return true
+  if (x < 0 || x >= rowLength)
+    return true
+  return false
+}
+
+const visit = (matrix, visited, basin, moob) => (x, y) => {
+  const isVisited = has(visited, [x,y])
+  if (moob(x, y) || isVisited) {
+    return
+  }
+
+  visited.add(p2s([x,y]))
+  const v = matrix[y][x]
+  if (v === 9)
+    return
+  
+  basin.add(p2s([x,y]))
+
+  visit(matrix, visited, basin, moob)(x, y-1)
+  visit(matrix, visited, basin, moob)(x-1, y)
+  visit(matrix, visited, basin, moob)(x+1, y)
+  visit(matrix, visited, basin, moob)(x, y+1)
+}
 
 const solve = data => {
-  const result = 0
+  const matrix = data.map(row => row.split('').map(s => parseInt(s)))
+  
+  const rows = data.length
+  const rowLength = first(data).length
+
+  const mLowest = isLowest(matrix, rows, rowLength)
+
+  let basins = []
+  
+  const moob = oob(matrix, rows, rowLength)
+  
+  let lowest = []
+  for (let r = 0; r < rows; ++r) {
+    for (let c = 0; c < rowLength; ++c) {
+      if (mLowest(c, r)) {
+        lowest.push(matrix[r][c])
+
+        let visited = new Set()
+        let basin = new Set()
+        visit(matrix, visited, basin, moob)(c,r)
+        basins.push(basin)
+      }
+    }
+  }
+
+  basins.sort((l, r) => r.size - l.size)
+  const result = basins[0].size * basins[1].size * basins[2].size
   return `Result: ${result}`
 }
 
-console.log("Test", solve(readInts('./test')))
-console.log("Data", solve(readInts('./data')))
+console.log("Test", solve(read('./test')))
+console.log("Data", solve(read('./data')))
