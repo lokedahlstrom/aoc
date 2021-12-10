216ac53e71aecde8083d1737135c424d0d483435
Loke Dahlström <L21390@eon.com>
2021-12-10 19:22:53 +0100

    Day 10, problem 1 and 2 solved

--- 2021/10/1.js
+++ 2021/10/1.js
@@ -1,9 +1,46 @@
-import { readInts } from '../helpers'
+import { read } from '../helpers'
+
+const expect = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>'
+}
+ 
+const cost = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137
+}
+ 
+const open  = new Set(Object.keys(expect))
+const close = new Set(Object.values(expect))
 
 const solve = data => {
-  const result = 0
+  let stack = []
+  let result = 0
+
+  data.forEach(row => {
+    row.split('').some(t => {
+      if (open.has(t)) {
+        stack.push(t)
+      } else if (close.has(t)) {
+        const o = stack.pop()
+        const expected = expect[o]
+        if (t !== expected) {
+          //console.log(`Expected ${expected}, but found ${t} instead`)
+          result += cost[t]
+          return true
+        }
+      }
+    
+      return false
+    })
+  })
+
   return `Result: ${result}`
 }
 
-console.log("Test", solve(readInts('./test')))
-console.log("Data", solve(readInts('./data')))
+console.log("Test", solve(read('./test')))
+console.log("Data", solve(read('./data')))
