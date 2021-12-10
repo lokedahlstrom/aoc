6d47767fa5dde7aaca77fe7d6e0db3891df26b59
Loke Dahlström <L21390@eon.com>
2021-12-10 19:24:12 +0100

    added stub for day 11

---
+++ 2021/11/2.js
@@ -0,0 +1,9 @@
+import { readInts } from '../helpers'
+
+const solve = data => {
+  const result = 0
+  return `Result: ${result}`
+}
+
+console.log("Test", solve(readInts('./test')))
+console.log("Data", solve(readInts('./data')))
