b673bdad1aacdab9e4f0e908cceb83540655410a
Loke Dahlström <loke.dahlstrom@gmail.com>
2022-12-06 17:56:04 +0100

    day six

---
+++ 2022/06/1.js
@@ -0,0 +1,28 @@
+import { readFileSync } from 'fs'
+
+export const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+const solve = lines => {
+  const window = 4
+
+  lines.forEach(line => {
+    line.split('').some((_, i) => {
+      let part = line.slice(i, i + window)
+      if (part.length === window) {
+        const letters = new Set(part)
+        if (letters.size === window) {
+          console.log(i + window)
+          return true
+        }
+      }
+    })
+  })
+  
+  return 0
+}
+
+console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('input'))}`)
