7a383ac0851516316600176875f0804707b5ce8f
Loke Dahlström <loke.dahlstrom@gmail.com>
2022-12-11 14:52:09 +0100

    day eleven

---
+++ 2022/11/2.js
@@ -0,0 +1,93 @@
+import { readFileSync } from 'fs'
+
+const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+const ints = s => s
+  .split('\n')
+  .map(line => line
+    .replace(/[\D]/g, ' ')  // replace non numeric with space
+    .replace(/\W+/g, ' ')   // remove repeated whitespaces
+    .split(' ')             // make array
+    .map(d => parseInt(d))  // make ints
+    .filter(d => !isNaN(d)) // remove any invalid numbers
+  )
+  .filter(a => a.length > 0)
+  .flat()
+
+const nonEmpty = s => s.length
+
+let monkeys = []
+let modolus = 1
+
+const inspect = (monkey, v) => {
+  const [_, op, rs] = monkey.op
+  const l = v
+  const r = rs === 'old' ? v : parseInt(rs)
+
+  monkey.inspections++
+
+  switch (op) {
+    case '+': return l + r
+    case '-': return l - r
+    case '*': return l * r
+    case '/': return l / r
+  }
+}
+
+const getBored = v => v % modolus
+
+const getThrowTarget = (monkey, v) => {
+  const newLevel = inspect(monkey, v)
+  const adjustedLevel = getBored(newLevel)
+  const divisible = adjustedLevel % monkey.test === 0
+  return {
+    target: divisible ? monkey.t : monkey.f,
+    value: adjustedLevel
+  }
+}
+
+const readMonkey = (lines) => {
+  const ops = lines[2].slice(19)
+  const op = ops.split(' ')
+
+  modolus *= ints(lines[3])[0]
+
+  monkeys.push({
+    id: ints(lines[0])[0],
+    items: ints(lines[1]),
+    op,
+    test: ints(lines[3])[0],
+    t: ints(lines[4])[0],
+    f: ints(lines[5])[0],
+    inspections: 0
+  })
+}
+
+const solve = lines => {
+  const x = lines.filter(nonEmpty)
+  for (let i = 0; i < x.length; i += 6) {
+    readMonkey(x.slice(i, i + 6))
+  }
+
+  for (let round = 0; round < 10000; ++round) {
+    monkeys.forEach(m => {
+      m.items.forEach(item => {
+        const { target, value } = getThrowTarget(m, item)
+        monkeys[target].items.push(value)
+      })
+
+      m.items = []
+    })
+  }
+
+  const inspections = monkeys.map(m => m.inspections).sort((a, b) => b - a)
+  return inspections[0] * inspections[1]
+}
+
+console.log(`Result: ${solve(read('test'))}`)
+monkeys = []
+modolus = 1
+console.log(`Result: ${solve(read('input'))}`)
