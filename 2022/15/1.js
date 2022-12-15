39c46b9b045d925abef9b3836bda21ed60bf3e87
Loke Dahlström <loke.dahlstrom@gmail.com>
2022-12-15 19:55:53 +0100

    15:1

---
+++ 2022/15/1.js
@@ -0,0 +1,54 @@
+import { readFileSync } from 'fs'
+import { ppid } from 'process'
+
+export const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+const nonEmpty = s => s.length
+const ints = s => s.match(/(\-?\d+)/g).map(d => parseInt(d))
+
+const md = (x1,y1,x2,y2) => Math.abs(x2-x1) + Math.abs(y2-y1)
+
+const sweep = (a,b,c,d,y) => {
+  let x = a
+  let beam = md(a,b,c,d)
+  let dist = md(a,b,x,y)
+  if (beam < dist) {
+    return []
+  }
+
+  let res = [x]
+
+  // left
+  while (beam >= md(a,b,--x,y)) {
+    res.push(x)
+  }
+  x = a
+  //right
+  while (beam >= md(a,b,++x,y)) {
+    res.push(x)
+  }
+
+  return res
+}
+
+const solve = (row, lines) => {
+  let set = new Set()
+  let pp = new Set()
+  lines.filter(nonEmpty).forEach(l => {
+    const [a,b,c,d] = ints(l)
+
+    if (d === row)
+      pp.add(c)
+
+    const reaches = sweep(a,b,c,d,row)
+    reaches.forEach(r => set.add(r))
+  })
+
+  return set.size - pp.size
+}
+
+console.log(`Result: ${solve(10, read('test'))}`)
+console.log(`Result: ${solve(2000000, read('input'))}`)
