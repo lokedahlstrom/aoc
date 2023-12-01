7c80800dfd7e7b5ec93968628f2e73cf781aff1e
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-01 11:46:43 +0100

    day one, part two

--- 2023/01/2.js
+++ 2023/01/2.js
@@ -10,15 +10,26 @@ const digits = s => s.split('').filter(isDigit)
 const first = v => v.at(0)
 const last = v => v.at(-1)
 
-const solve = lines => {
-  
+const fix = s => {
+  return s.replaceAll('one', 'one1one')
+    .replaceAll('two', 'two2two')
+    .replaceAll('three', 'three3three')
+    .replaceAll('four', 'four4four')
+    .replaceAll('five', 'five5five')
+    .replaceAll('six', 'six6six')
+    .replaceAll('seven', 'seven7seven')
+    .replaceAll('eight', 'eight8eight')
+    .replaceAll('nine', 'nine9nine')
+}
+
+const solve = lines => {  
   return lines.reduce((acc, l) => {
-     const d = digits(l)
+     const d = digits(fix(l))
      const num = Number(first(d) + last(d))
      
      return acc += isNaN(num) ? 0 : num
   }, 0)
 }
 
-console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('test2'))}`)
 console.log(`Result: ${solve(read('input'))}`)
