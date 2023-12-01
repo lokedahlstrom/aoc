713b6c1ecba0fa8e72154f345eb443e7d0b70825
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-01 10:00:41 +0100

    day one, task one

---
+++ 2023/1.js
@@ -0,0 +1,24 @@
+import { readFileSync } from 'fs'
+
+export const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+const isDigit = c => c >= '0' && c <= '9'
+const digits = s => s.split('').filter(isDigit)
+const first = v => v.at(0)
+const last = v => v.at(-1)
+
+const solve = lines => {
+  
+  return lines.reduce((acc, l) => {
+     const d = digits(l)
+     const num = Number(first(d) + last(d))
+     
+     return acc += isNaN(num) ? 0 : num
+  }, 0)
+}
+
+console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('input'))}`)
