613752abb4283a4cceeb2d29c7a3a9513c6f84a9
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-02 11:38:30 +0100

    day three prepared

---
+++ 2023/03/1.js
@@ -0,0 +1,13 @@
+import { readFileSync } from 'fs'
+
+export const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+const solve = lines => {
+  return 0
+}
+  
+console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('input'))}`)
