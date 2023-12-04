8e3760c75a49cd90bb1ad35f476a290a1894d728
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-04 14:39:02 +0100

    day four, both parts

---
+++ 2023/04/1.js
@@ -0,0 +1,25 @@
+import { readFileSync } from 'fs'
+
+export const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+const numbers = s => s.match(/\d+/g).map(s => Number(s))
+const power = n => n < 0 ? 0 : Math.pow(2, n)
+  
+const solve = lines => {
+  return lines.map(l => l.substring(8).split('|'))
+    .map(row => row.map(numbers))
+    .map(([left, right]) => {
+      const ls = new Set([...left])
+      const rs = new Set([...right])
+
+      const inLeft = [...rs.intersection(ls)]
+      return inLeft
+    })
+    .reduce((acc, list) => acc + power(list.length - 1), 0)
+}
+
+console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('input'))}`)
---
+++ 2023/04/2.js
@@ -0,0 +1,36 @@
+import { readFileSync } from 'fs'
+
+export const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n')
+}
+
+const numbers = s => s.match(/\d+/g).map(s => Number(s))
+const range = n => [...Array(n).keys()]
+  
+const solve = lines => {
+  const cardCount = {}
+  range(lines.length).forEach(n => cardCount[n+1] = 1)
+  lines.map(l => l.substring(8).split('|'))
+    .map(row => row.map(numbers))
+    .map(([left, right], i) => {
+      const currentCard = i + 1
+      const ls = new Set([...left])
+      const rs = new Set([...right])
+
+      const inLeft = [...rs.intersection(ls)]
+
+      range(cardCount[currentCard]).forEach(_ => {
+        range(inLeft.length).forEach(n => {
+          cardCount[currentCard+1+n] += 1
+        })
+      })
+      
+      return inLeft
+    })
+
+  return Object.values(cardCount).reduce((acc, cur) => acc + cur, 0)
+}
+
+console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('input'))}`)
