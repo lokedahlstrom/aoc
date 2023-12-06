227ff9c3ca4e53cead6f5b67fc70308dfdf40911
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-06 22:59:36 +0100

    day five, part one

--- 2023/05/2.js
+++ 2023/05/2.js
@@ -5,10 +5,25 @@ export const read = file => {
   return text.split('\n')
 }
 
-const first = a => a.at(0)
 const toNumber = s => Number(s)
 const notEmpty = s => s.trim() !== ''
 const isDigit = c => c >= '0' && c <= '9'
+const isEven = n => n % 2 === 0
+
+class Range {
+  constructor(low, range) {
+    this.low = low
+    this.high = low + range
+  }
+
+  in(other) {
+    return this.low >= other.low && this.high <= other.high
+  }
+
+  print() {
+    return '[' + this.low + ', ' + this.high + ']'
+  }
+}
   
 const getSectionMap = (lines, start, dict) => {
   let line = lines[start]
@@ -29,20 +44,23 @@ const getSectionMap = (lines, start, dict) => {
   return start
 }
 
-const getDestination = (key, dict) => {
+const getDestination = (seedRange, dict) => {
   const keys = Object.keys(dict)
-  const source = Number(key)
-  let res = source
+  let res = seedRange
 
   keys.map(toNumber).some(k => {
     const { destination, range } = dict[k]
     const num = Number(k)
+    const sourceRange = new Range(num, range)
+    //const destRange = new Range(destination, range)
+    const delta = destination - num
+    console.log('delta', delta)
 
-    if (source >= num && source <= (num + range)) {
-      const delta = source - num
-      res = destination + delta
+    if (seedRange.in(sourceRange)) {
+      res = new Range(destination + delta, 0)
       return true
     }
+
     return false
   })
 
@@ -60,7 +78,15 @@ const solve = input => {
   const temperatureToHumidity = {}
   const humidityToLocation = {}
 
-  const seeds = lines[0].substring(7).split(' ')
+  const seeds = lines[0].substring(7)
+    .split(' ')
+    .map(toNumber)
+    .reduce((acc, cur, i, arr) => {
+      if (isEven(i)) {
+        acc.push(arr.slice(i, i + 2))
+      }
+      return acc
+    }, [])
 
   let start = 2
   start = getSectionMap(lines, start, seedToSoil)
@@ -71,20 +97,39 @@ const solve = input => {
   start = getSectionMap(lines, start+1, temperatureToHumidity)
   start = getSectionMap(lines, start+1, humidityToLocation)
 
-  const res = seeds.map(seed => {
-    const soil  = getDestination(seed, seedToSoil)
+  //  const res = seeds.map(pair => {
+  //   const range = new Range(pair[0], pair[1])
+  //   console.log(range.print())
+  //   const soil  = getDestination(range, seedToSoil)
+  //   console.log('soil', soil.print())
+  //   const fert  = getDestination(soil, soilToFertilizer)
+  //   const water = getDestination(fert, fertilizerToWater)
+  //   const light = getDestination(water, waterToLight)
+  //   const temp  = getDestination(light, lightToTemperature)
+  //   const humid = getDestination(temp, temperatureToHumidity)
+  //   return getDestination(humid, humidityToLocation)
+  // })
+
+    const range = new Range(79, 14)
+    const soil  = getDestination(range, seedToSoil)
+    console.log('soilToFertilizer')
     const fert  = getDestination(soil, soilToFertilizer)
+    console.log('fertilizerToWater')
     const water = getDestination(fert, fertilizerToWater)
+    console.log('waterToLight')
     const light = getDestination(water, waterToLight)
+    console.log('lightToTemperature')
     const temp  = getDestination(light, lightToTemperature)
+    console.log('temperatureToHumidity')
     const humid = getDestination(temp, temperatureToHumidity)
+    console.log('humidityToLocation')
     const loc   = getDestination(humid, humidityToLocation)
 
-    return loc
-  })
+  console.log(soil.low, fert.low, water.low, light.low, temp.low, humid.low, loc.low)
 
-  return Math.min(...res)
+  // res.sort((a,b) => a.low - b.low)
+  // console.log(res)
 }
 
 console.log(`Result: ${solve(read('test'))}`)
-console.log(`Result: ${solve(read('input'))}`)
+//console.log(`Result: ${solve(read('input'))}`)
