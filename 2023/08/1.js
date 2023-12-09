4d9bdd4f7e40447a9014e69b78ee4c7709c11fea
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-09 13:48:44 +0100

    day eight, part one

--- 2023/08/1.js
+++ 2023/08/1.js
@@ -6,8 +6,31 @@ export const read = file => {
 }
 
 const solve = input => {
+  const map = {}
+  const directions = input[0]
+  
+  input.slice(2).forEach(line =>  {
+    const [source, left, right] = line.match(/\w+/g)
+    map[source] = [left, right]
+  })
+  
+  let current = 'AAA'
+  let i = 0
+  while (true) {
+    const walk = directions[i % directions.length]
+    const destination = map[current][walk === 'L' ? 0 : 1]
+
+    if (destination === 'ZZZ') {
+      return i+1
+    }
+    
+    current = destination
+    ++i
+  }
+
   return 0
 }
 
 console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('test2'))}`)
 console.log(`Result: ${solve(read('input'))}`)
