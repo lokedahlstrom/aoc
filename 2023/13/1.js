2a56ad30767014cb1ab72df52e521f6cebfb8ac2
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-13 21:18:23 +0100

    day thirteen, both parts

---
+++ 2023/13/1.js
@@ -0,0 +1,42 @@
+import { readFileSync } from 'fs'
+
+const read = file => {
+  const text = readFileSync(file).toString('utf-8')
+  return text.split('\n\n')
+}
+
+const rotateCCW = m => {
+  let transposed = []
+  for (let y in m) {
+    for (let x in m[y]) {
+      transposed[x] = transposed[x] || "";
+      transposed[x] += m[y][x];
+    }
+  }
+  return transposed
+}
+
+const checkMirror = block => {
+  for (let i = 0; i < block.length - 1; i++) {
+    let diffCount = 0
+
+    for (let j = 0; j <= Math.min(i, block.length - i - 2); j++)
+      diffCount += block[i - j]
+        .split('').
+        filter((c, idx) => c !== block[i + 1 + j][idx]).length
+
+    if (diffCount === 0)
+      return i + 1
+  }
+  return 0
+}
+
+const solve = blocks => {
+  return blocks.reduce((acc, blockLine) => {
+    const block = blockLine.split('\n')
+    return acc + checkMirror(block) * 100 + checkMirror(rotateCCW(block))
+  }, 0)
+}
+
+console.log(`Result: ${solve(read('test'))}`)
+console.log(`Result: ${solve(read('input'))}`)
