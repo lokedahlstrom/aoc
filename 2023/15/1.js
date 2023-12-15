a62e74b9d9cb5cbdb6d558da094c512314ca3266
Loke Dahlström <loke.dahlstrom@gmail.com>
2023-12-15 19:57:51 +0100

    day fifteen, part one

--- 2023/15/1.js
+++ 2023/15/1.js
@@ -1,12 +1,18 @@
 import { readFileSync } from 'fs'
 
-export const read = file => {
-  const text = readFileSync(file).toString('utf-8')
-  return text.split('\n')
+const read = file => readFileSync(file).toString('utf-8')
+
+const hash = s => {
+  return s.split('').reduce((acc, c) => {
+    acc += c.charCodeAt(0)
+    acc *= 17
+    acc %= 256
+    return acc
+  }, 0)
 }
 
-const solve = lines => {
-  return 0
+const solve = line => {
+  return line.split(',').reduce((acc, s) => acc + hash(s), 0)
 }
 
 console.log(`Result: ${solve(read('test'))}`)
